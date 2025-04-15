6586acf4c1ba4fcd874b5bbcc7965c0fc2363235
dan-lk7 <nyemabudan@gmail.com>
2025-04-15 23:13:19 +0300

    ajout du render.yaml

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        if ($url = env('DATABASE_URL')) {
+            $components = parse_url($url);
+            config([
+                'database.connections.mysql.host' => $components['host'],
+                'database.connections.mysql.database' => ltrim($components['path'], '/'),
+                'database.connections.mysql.username' => $components['user'],
+                'database.connections.mysql.password' => $components['pass'],
+            ]);
+        }
     }
 }
